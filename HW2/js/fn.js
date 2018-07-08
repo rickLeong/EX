986ac4ce002dac89756306c6d4bd6e9df3483306
rickLeong <31367402+rickLeong@users.noreply.github.com>
2018-07-08 20:49:59 +0800

    final

--- HW2/js/fn.js
+++ HW2/js/fn.js
@@ -8,9 +8,10 @@
         increaseBtn = document.querySelectorAll('.increaseBtn'),
         decreaseBtn = document.querySelectorAll('.decreaseBtn'),
         price = document.querySelectorAll('.price_num'),
-        total_price = document.querySelectorAll('.total_price'),
+        total_price = document.querySelectorAll('.total_price_num'),
         result = document.querySelector('.result');
 
+    console.log(selectOne);
 
     //點擊全選按鈕時全選複選框
     (selectAll.onclick = function () {
@@ -19,13 +20,14 @@
                 selectOne[i].checked = true;
                 //全選時全部防入selectedItem對象中
                 selectedItem['checkbox'+i] = i;
+                update_totalPrice(i);
             } else {
                 selectOne[i].checked = false;
                 //選中狀態點擊時刪除所有selectedItem對象中的值
                 delete selectedItem['checkbox'+i]
+                update_totalPrice(i);
             }
         }
-        console.log(selectedItem);
     })();
 
 
@@ -41,9 +43,9 @@
                 selectAll.checked = true;
             }
             console.log(selectedItem);
+            update_totalPrice(i);
         }
     }
-
 
 
     for(let i=0;i<cart_product.length;i++){
@@ -53,23 +55,38 @@
         };
         decreaseBtn[i].onclick = function () {
             quantityInput[i].value = +quantityInput[i].value - 1;
-            if(quantityInput[i].value < 1) return quantityInput[i].value=1;
+            if(quantityInput[i].value < 1)  quantityInput[i].value = 1;
             update_totalPrice(i)
         };
     }
 
+    for(let i = 0;i < quantityInput.length;i++){
+        quantityInput[i].onblur = function(){
+            if(quantityInput[i].value < 1) quantityInput[i].value= 1;
+            update_totalPrice(i);
+        };
+    }
 
 
      function calculate(product_num) {
          return quantityInput[product_num].value * price[product_num].innerHTML;
      }
 
-     function resultCalculate(){
-
+     function resultCalulate(){
+         let sum = 0;
+         console.log(selectedItem);
+         for(let p in selectedItem){
+             sum += +total_price[selectedItem[p]].innerHTML;
+             console.log(p);
+         }
+         result.innerHTML = sum;
      }
 
+
+
      function update_totalPrice(product_num){
-        total_price[product_num].innerHTML = "￥"+calculate(product_num);
+        total_price[product_num].innerHTML = calculate(product_num);
+        resultCalulate();
      }
 
 
